f4278cd6e12bdeefd9a93effd8e77a37993410e1
eunjin <dmswls1107@gmail.com>
2022-09-26 14:21:34 +0900

    update

--- src/main/java/com/example/studyboard/Controller/BoardController.java
+++ src/main/java/com/example/studyboard/Controller/BoardController.java
@@ -3,13 +3,12 @@ package com.example.studyboard.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/board/**") // /board로 접근하는 url처리를 이 클래스인 boardController에서 맡는다고 알려줌
+@RestController
 public class BoardController {
-
-    @GetMapping("/hello")
+    @RequestMapping("/")
     public String Hello(){
-        return "/boards/hello";
+        return "helvvlo";
     }
 }
---
+++ src/main/java/com/example/studyboard/model/Member.java
@@ -0,0 +1,4 @@
+package com.example.studyboard.model;
+
+public class Member {
+}
